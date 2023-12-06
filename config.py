75beb55f63528fdbf13c76001955f1f975d9c850
raza4729 <raza4729@gmail.com>
2023-12-06 20:12:04 +0100

    cache path added

--- config.py
+++ config.py
@@ -1,12 +1,15 @@
 from transformers import TrainingArguments
 
 
+# Please safely remore the following path and add path where you want to store the data from hugginface and models
+path = "/mount/studenten/arbeitsdaten-studenten1/razaai/cache"
+
 training_args = TrainingArguments(
-    output_dir="named-entity-recognition-multinerd",
+    output_dir="named-entity-recognition-distilbert-B",
     learning_rate=2e-5,
-    per_device_train_batch_size=16,
-    per_device_eval_batch_size=8,
-    num_train_epochs=2,
+    per_device_train_batch_size=32,
+    per_device_eval_batch_size=16,
+    num_train_epochs=3,
     weight_decay=0.01,
     evaluation_strategy="epoch",
     save_strategy="epoch",
